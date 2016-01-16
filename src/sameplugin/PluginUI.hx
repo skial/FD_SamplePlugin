1f5010acdca986438deebc3ace15a29a2953d9fd
Skial Bainn <skial.bainn@gmail.com>
2016-01-16 15:07:11 +0000

    Add `@:nativeGen` when extending native types. Add `@:isVar` for real c# properties.

--- src/sameplugin/PluginUI.hx
+++ src/sameplugin/PluginUI.hx
@@ -1,15 +1,27 @@
 package sameplugin;
 
 import plugincore.*;
-import cs.system.windows.forms.*;
+import cs.system.windows.forms.RichTextBox;
+import cs.system.windows.forms.UserControl;
 
 /**
  * ...
  * @author Skial Bainn
  */
-class PluginUI {
+@:nativeGen class PluginUI extends UserControl {
+	
+	@:property @:isVar private var richTextBox(get, never):RichTextBox;
+	private var pluginMain:PluginMain;
 
-	public function new() {
+	public function new(pluginMain:PluginMain) {
+		super();
+	}
+	
+	private function get_richTextBox():RichTextBox {
+		return this.richTextBox;
+	}
+	
+	private function InitializeComponent():Void {
 		
 	}
 	
