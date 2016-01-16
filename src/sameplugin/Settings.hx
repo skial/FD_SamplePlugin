e8f36f3312024e598eb1abb1234632abbec4247e
Skial Bainn <skial.bainn@gmail.com>
2016-01-16 15:50:15 +0000

    `@:strict` metadata works for simple expressions, fails on `Keys.Control | Keys.F1`. Resort to long `@:meta` alternative.

--- src/sameplugin/Settings.hx
+++ src/sameplugin/Settings.hx
@@ -2,6 +2,8 @@ package sameplugin;
 
 import cs.system.windows.forms.Keys;
 import cs.system.SerializableAttribute;
+import cs.system.componentmodel.DescriptionAttribute;
+import cs.system.componentmodel.DefaultValueAttribute;
 
 /**
  * ...
@@ -11,12 +13,50 @@ import cs.system.SerializableAttribute;
 @:strict(SerializableAttribute)
 class Settings {
 	
-	private var samepleNumber:Int = 69;
-	private var sampleText:String = 'This is a sample plugin.';
+	@:isVar
+	@:property
+	@:strict(DescriptionAttribute("A sample integer setting."))
+	@:strict(DefaultValueAttribute(69))
+	private var sampleNumber:Int = 69;
+	
+	@:isVar
+	@:property
+	@:strict(DescriptionAttribute("A sample string setting."))
+	@:strict(DefaultValueAttribute("This is a sample plugin."))
+	private var sampleText(get, set):String = 'This is a sample plugin.';
+	
+	@:isVar
+	@:property
+	@:strict(DescriptionAttribute("A sample shortcut setting."))
+	@:meta("System.ComponentModel.DefaultValueAttribute(System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F1)")
 	private var sampleShortcut:Keys = untyped Keys.Control | Keys.F1;
-
+	
 	public function new() {
 		
+	}
+	
+	private function get_sampleText():String {
+		return this.sampleText;
+	}
+	
+	private function set_sampleText(value:String):String {
+		return this.sampleText = value;
+	}
+	
+	private function get_sampleNumber():Int {
+		return this.sampleNumber;
+	}
+	
+	private function set_sampleNumber(value:Int):Int {
+		return this.sampleNumber = value;
+	}
+	
+	private function get_sampleShortcut():Keys {
+		return this.sampleShortcut;
+	}
+	
+	private function set_sampleShortcut(value:Keys):Keys {
+		return this.sampleShortcut = value;
 	}
 	
 }
