af97ca6c9d2dd8f75864054279bc0a56b5aa57f3
Skial Bainn <skial.bainn@gmail.com>
2016-01-16 15:25:29 +0000

    To add native meta use `@:meta` with the full path to meta, eg `System.Serializable`.

--- src/sameplugin/Settings.hx
+++ src/sameplugin/Settings.hx
@@ -1,14 +1,19 @@
 package sameplugin;
 
+import cs.system.windows.forms.Keys;
+
 /**
  * ...
  * @author Skial Bainn
  */
-class Settings
-{
+@:meta('System.Serializable')
+class Settings {
+	
+	private var samepleNumber:Int = 69;
+	private var sampleText:String = 'This is a sample plugin.';
+	private var sampleShortcut:Keys = untyped Keys.Control | Keys.F1;
 
-	public function new() 
-	{
+	public function new() {
 		
 	}
 	
