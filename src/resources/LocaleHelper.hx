797769260fe310fbf4a8673b89e3aae87ab0b31b
Skial Bainn <skial.bainn@gmail.com>
2016-01-22 11:06:18 +0000

    **No errors** using a standalone git built version of FlashDevelop!

--- src/resources/LocaleHelper.hx
+++ src/resources/LocaleHelper.hx
@@ -1,7 +1,9 @@
 package resources;
 
+import cs.system.Object;
 import cs.system.reflection.Assembly;
 import cs.system.resources.ResourceManager;
+import haxe.PosInfos;
 import plugincore.localization.LocaleVersion;
 
 /**
@@ -16,13 +18,14 @@ class LocaleHelper {
 		
 	}
 	
-	public static function Initialize(locale:LocaleVersion):Void {
-		var path = 'SamplePlugin.Resources.' + locale;//.ToString();
-		//resources = new ResourceManager(path, Assembly.GetExecutingAssembly());
-		resources = ResourceManager.CreateFileBasedResourceManager('en_US.resX', './', null);
+	public static function Initialize(locale:LocaleVersion, ?pos:PosInfos):Void {
+		var path = 'SamplePlugin.Resources.en_US';
+		resources = new ResourceManager(path, Assembly.GetExecutingAssembly());
+		//throw ([resources, path, locale, pos]:Array<Dynamic>);
+		//resources = ResourceManager.CreateFileBasedResourceManager('en_US.resX', './', null);
 	}
 	
-	public static function GetString(identifier:String):String {
+	public static function GetString(identifier:String, ?pos:PosInfos):String {
 		return resources.GetString(identifier);
 	}
 	
