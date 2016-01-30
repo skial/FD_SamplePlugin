cad07a28b3f841c4662ab90646b8ec0eb6861556
Skial Bainn <skial.bainn@gmail.com>
2016-01-30 10:54:37 +0000

    Add DisplayName attribute. Displays in FD settings. Access private vars as `@:isVar` and `@:strict(DisplayNameAttribute(''))` causes a compiler error. :/

--- src/sampleplugin/Settings.hx
+++ src/sampleplugin/Settings.hx
@@ -2,6 +2,7 @@ package sampleplugin;
 
 import cs.system.windows.forms.Keys;
 import cs.system.SerializableAttribute;
+import cs.system.componentmodel.DisplayNameAttribute;
 import cs.system.componentmodel.DescriptionAttribute;
 import cs.system.componentmodel.DefaultValueAttribute;
 
@@ -12,51 +13,54 @@ import cs.system.componentmodel.DefaultValueAttribute;
 // @see https://github.com/HaxeFoundation/haxe/pull/3954
 @:strict(SerializableAttribute)
 @:nativeGen class Settings {
-	
-	@:isVar
+
 	@:property
+	@:strict(DisplayNameAttribute("Sample Number"))
 	@:strict(DescriptionAttribute("A sample integer setting."))
 	@:strict(DefaultValueAttribute(69))
-	public var sampleNumber:Int = 69;
-	
-	@:isVar
+	public var sampleNumber(get, set):Int;
+	private var _sampleNumber = 69;
+
 	@:property
+	@:strict(DisplayNameAttribute("Sample Text"))
 	@:strict(DescriptionAttribute("A sample string setting."))
 	@:strict(DefaultValueAttribute("This is a sample plugin."))
-	public var sampleText(get, set):String = 'This is a sample plugin.';
-	
-	@:isVar
+	public var sampleText(get, set):String;
+	private var _sampleText:String = 'This is a sample plugin.';
+
 	@:property
+	@:strict(DisplayNameAttribute("Sample Shortcut"))
 	@:strict(DescriptionAttribute("A sample shortcut setting."))
 	@:meta("System.ComponentModel.DefaultValueAttribute(System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F1)")
-	public var sampleShortcut:Keys = untyped Keys.Control | Keys.F1;
-	
+	public var sampleShortcut(get, set):Keys;
+	private var _sampleShortcut:Keys = untyped Keys.Control | Keys.F1;
+
 	public function new() {
-		
+
 	}
-	
+
 	private function get_sampleText():String {
-		return this.sampleText;
+		return this._sampleText;
 	}
-	
+
 	private function set_sampleText(value:String):String {
-		return this.sampleText = value;
+		return this._sampleText = value;
 	}
-	
+
 	private function get_sampleNumber():Int {
-		return this.sampleNumber;
+		return this._sampleNumber;
 	}
-	
+
 	private function set_sampleNumber(value:Int):Int {
-		return this.sampleNumber = value;
+		return this._sampleNumber = value;
 	}
-	
+
 	private function get_sampleShortcut():Keys {
-		return this.sampleShortcut;
+		return this._sampleShortcut;
 	}
-	
+
 	private function set_sampleShortcut(value:Keys):Keys {
-		return this.sampleShortcut = value;
+		return this._sampleShortcut = value;
 	}
-	
+
 }
